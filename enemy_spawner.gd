67259ddd9259e66c6952ea377fa423333d91c607
Lunar <lunarlifeburst@gmail.com>
2026-07-31 13:26:16 +1000

    many enemies spawned

---
+++ enemy_spawner.gd
@@ -0,0 +1,18 @@
+extends Node
+
+const enemy = preload("res://enemy.tscn")
+
+var time = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	time = time + delta
+	if time > 0.5:
+		print("D")
+		$"..".add_child(enemy.instantiate())
+		time = 0
