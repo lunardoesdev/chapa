98dd36255169507b7d5101d56d4eca086527a230
Lunar <lunarlifeburst@gmail.com>
2026-08-01 21:35:55 +1000

    esc to exit

--- game.gd
+++ game.gd
@@ -40,3 +40,7 @@ func _process(delta: float) -> void:
 		time = 0
 		
 		# print("enemies on map: ", get_tree().get_nodes_in_group("enemies").size())
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().quit()
